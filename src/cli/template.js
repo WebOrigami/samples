edfeae9c1b06619f4a1c62cbcfb510d4ccc010af
Jan Miksovsky <jan@miksovsky.com>
2023-05-16 10:05:09 -0700

    Add all other samples from docs project

---
+++ src/cli/template.js
@@ -0,0 +1,16 @@
+export default (body) => `<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      body {
+        font-family: Zapfino, Segoe Script, cursive;
+        color: darkred;
+        font-size: 48px;
+      }    
+    </style>
+  </head>
+  <body>
+    ${body}
+  </body>
+</html>
+`;
---
+++ src/cli/text.js
@@ -0,0 +1,3 @@
+export default function text(obj) {
+  return Object.values(obj).join("\t");
+}
